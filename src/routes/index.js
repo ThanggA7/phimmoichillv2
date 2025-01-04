c34a5a40f21b74c7e575bc63cd07284585ab0c58
Thangga7 <beencules@gmail.com>
2025-01-05 00:05:19 +0700

    ADD

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import Category from "../Pages/Category/Category";
 import Home from "../Pages/Home/Home";
 import Info from "../Pages/Info/Info";
 import Watch from "../Pages/Watch/Watch";
+import NOTFOUND from "../Pages/404/404";
 
 const routePublic = [
   { path: "/", component: Home },
@@ -11,6 +12,7 @@ const routePublic = [
   { path: "/watch/:id/", component: Watch },
   { path: "/quoc-gia/:id", component: Country },
   { path: "/the-loai/:id", component: Category },
+  { path: "*", component: NOTFOUND },
 ];
 
 export default routePublic;
